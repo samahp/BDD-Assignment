38f959588a95665a67c7625ae94d7cc02b1c1059
samahp <sama@gatech.edu>
2015-11-22 18:30:19 -0500

    Modified steps.py

--- tests/features/steps.py
+++ tests/features/steps.py
@@ -1,31 +1,36 @@
 from lettuce import *
 from nose.tools import assert_equals
 from app.calculator import Calculator
- 
- 
+
+
 @step(u'I am using the calculator')
 def select_calc(step):
 #    print ('Attempting to use calculator...')
     world.calc = Calculator()
- 
- 
+
+
 @step(u'I input "([^"]*)" add "([^"]*)"')
 def step_for_add(step, x, y):
     world.result = world.calc.add(int(x), int(y))
- 
+
 
 @step(u'I input "([^"]*)" multiply "([^"]*)"')
 def step_for_mutiply(step, x, y):
     world.result = world.calc.multiply(int(x), int(y))
 
 
-'''
-#TODO finish step for volume
+
+@step(u'I input Calculate volume of "([^"]*)", "([^"]*)", "([^"]*)"')
 def step_for_volume(step, x, y, z):
-    
-#TODO finish step for factorial
+
+    world.result = world.calc.volume(int(x), int(y), int(z))
+
+
+
+@step(u'I input compute factorial of "([^"]*)"')
 def step_for_factorial(step, x):
-'''
+     world.result = world.calc.factorial(int(x))
+
 
 
 @step(u'I should see "([^"]+)"')
@@ -35,7 +40,3 @@ def result(step, expected_result):
         assert(actual_result is None)
     else:
         assert_equals(int(expected_result), actual_result)
-
-
-
-
