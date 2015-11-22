23910ebf58efdf38ce965db98de3e72dcf0f807f
samahp <sama@gatech.edu>
2015-11-22 18:22:06 -0500

    Modified calculator.py

--- app/calculator.py
+++ app/calculator.py
@@ -1,7 +1,7 @@
 class Calculator(object):
     def add(self, x, y):
         number_types = (int, long, float, complex)
- 
+
         if isinstance(x, number_types) and isinstance(y, number_types):
             return x + y
         else:
@@ -10,7 +10,7 @@ class Calculator(object):
 
     def multiply(self, x, y):
         number_types = (int, long, float, complex)
- 
+
         if isinstance(x, number_types) and isinstance(y, number_types):
             return x * y
         else:
@@ -19,22 +19,24 @@ class Calculator(object):
     def volume(self, l, h, b):
         number_types = (int, long, float, complex)
         if isinstance(l, number_types) and isinstance(h, number_types) and isinstance(b, number_types):
-            #TODO compute volume here
-            return 0
+            return l * h * b
         else:
             raise ValueError
 
 
     def factorial(self, n):
         number_types = (int, long)
- 
+
         if isinstance(n, number_types):
             if n < 0:
                 return None
             else:
-                #TODO compute factorial here
-                return 0
+                factorial = 1
+                if (n == 0) :
+                  return 1
+                else:
+                    for i in range(1, n + 1):
+                        factorial = factorial * i
+                    return factorial
         else:
             raise ValueError
-
-    
